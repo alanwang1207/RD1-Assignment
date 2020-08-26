ddf533ca9ecef4e49c2361f2a58a69211ba511f9
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-26 17:34:24 +0800

    測試get

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+$(function () {
+    $.ajax({
+      type: "GET",
+      url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-001?Authorization=CWB-20260A47-5D47-474D-AABA-BBC6BC84F310&format=JSON&sort=time",
+      dataType: "json",
+      error: function (e) {
+        console.log('oh no');
+      },
+      success: function (e) {
+        var xml = e;
+        console.log($(xml));
+      }
+    });
+  });
