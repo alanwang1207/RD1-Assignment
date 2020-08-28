cf80749885fbd86ffeae2f0e21104f609eb73efc
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-28 15:40:16 +0800

    新增圖片_景點

--- test.php
+++ test.php
@@ -1,19 +1,47 @@
-<?php
-    $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-0EF10C78-E76B-49E3-BD74-05B21416C3F5&format=JSON";  // Your json data url
-    $data = file_get_contents($url);  // PHP get data from url
-    $json = json_decode($data, true);  // Decode json data    
-    var_dump($json['records']['location'][0]['locationName']);// 查詢資料
-    echo "<br>";
-    var_dump($json['records']['location'][0]['weatherElement'][0]['time'][0]['parameter']['parameterName']);
- 
-?>
+<!-- <?php
+        $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-20260A47-5D47-474D-AABA-BBC6BC84F310&format=JSON&sort=time";  // Your json data url
+        $data = file_get_contents($url);  // PHP get data from url
+        $json = json_decode($data, true);  // Decode json data
+        for ($i = 0; $i < 22; $i++) {
+            echo "城市名 : " . $json['records']['location'][$i]['locationName'];
+            // var_dump();// 查詢資料
+            echo "<br>";
+            for ($j = 0; $j < 5; $j++) {
+                echo $json['records']['location'][$i]['weatherElement'][$j]['elementName'] . " : " . $json['records']['location'][$i]['weatherElement'][$j]['time'][0]['parameter']['parameterName'];
+                echo "<br>";
+            }
+            echo "<br>";
+        }
+
+
+
+        ?> -->
+
 
 <?php
-    $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-003?Authorization=CWB-0EF10C78-E76B-49E3-BD74-05B21416C3F5&format=JSON";  // Your json data url
-    $data = file_get_contents($url);  // PHP get data from url
-    $json = json_decode($data, true);  // Decode json data    
-    var_dump($json['records']['location'][0]['locationName']);// 查詢資料
+date_default_timezone_set('Asia/Taipei');
+$date = date("Y-m-d H:i:s");
+echo $date;
+$url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-20260A47-5D47-474D-AABA-BBC6BC84F310&format=JSON&sort=time";  // Your json data url
+$data = file_get_contents($url);  // PHP get data from url
+$json = json_decode($data, true);  // Decode json data
+
+
+for ($i = 0; $i < 22; $i++) {
+    echo "城市名 : " . $json['records']['locations'][0]['location'][$i]['locationName'];
+    // var_dump();// 查詢資料
     echo "<br>";
-    var_dump($json['records']['location'][0]['weatherElement'][0]['time'][0]['parameter']['parameterName']);
- 
+    for ($j = 0; $j < 15; $j++) {
+        for($k = 0;$k<14;$k++){
+            echo $json['records']['locations'][0]['location'][$i]['weatherElement'][$j]["description"] . " : " . $json['records']['locations'][0]['location'][$i]['weatherElement'][$j]["time"][$k]["elementValue"][0]["value"];
+            echo "<br>";
+        }
+
+           
+        
+    }
+    echo "<br>";
+}
+
+
 ?>
