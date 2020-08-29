b7921f91e97db3c47eec55172f51c075a6caebd9
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-29 13:59:03 +0800

    印出即時天氣_ok

--- current_weather.php
+++ current_weather.php
@@ -9,32 +9,62 @@ $api = "F-C0032-001";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
 
-$url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/".$api."?Authorization=".$Authorization."&format=JSON&locationName=".$locationName."&sort=time";  // Your json data url
+$url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $api . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&sort=time";  // Your json data url
 $json = file_get_contents($url);  // 把整個文件讀入一個字符串中
 $data = json_decode($json, true);  // 將json轉成陣列或object 
 // var_dump($data);
 $weatherElement = $data['records']['location'][0]['weatherElement'];
 // unset($json, $data);
 // var_dump(count($weatherElement));//記錄天氣因子個數
-for($i=0;$i<count($weatherElement);$i++){
+for ($i = 0; $i < count($weatherElement); $i++) {
     $elementName = $weatherElement[$i]["elementName"];
     echo "<br>";
     $time = $weatherElement[$i]["time"];
-        echo $elementName." : ".$parameterName = $time[0]["parameter"]["parameterName"];
-        echo "<br>";
-        echo "startTime : ".$startTime = $time[0]["startTime"];
-        echo "<br>";
-        echo "endTime : ".$endTime = $time[0]["endTime"];
-        echo "<br>";
-        
-
-    
+    $parameterName = $time[0]["parameter"]["parameterName"];
+    switch ($elementName) {
+        case "Wx":
+            echo "目前天氣狀況 : " . $parameterName;
+            echo "<br>";
+            echo "startTime : " . $startTime = $time[0]["startTime"];
+            echo "<br>";
+            echo "endTime : " . $endTime = $time[0]["endTime"];
+            echo "<br>";
+            break;
+        case "PoP":
+            echo "降雨機率 : " . $parameterName." %";
+            echo "<br>";
+            echo "startTime : " . $startTime = $time[0]["startTime"];
+            echo "<br>";
+            echo "endTime : " . $endTime = $time[0]["endTime"];
+            echo "<br>";
+            break;
+        case "MinT":
+            echo "最低溫度 : " . $parameterName." C";
+            echo "<br>";
+            echo "startTime : " . $startTime = $time[0]["startTime"];
+            echo "<br>";
+            echo "endTime : " . $endTime = $time[0]["endTime"];
+            echo "<br>";
+            break;
+        case "MaxT":
+            echo "最高溫度 : " . $parameterName." C";
+            echo "<br>";
+            echo "startTime : " . $startTime = $time[0]["startTime"];
+            echo "<br>";
+            echo "endTime : " . $endTime = $time[0]["endTime"];
+            echo "<br>";
+            break;
+        case "CI":
+            echo "舒適度 : " . $parameterName;
+            echo "<br>";
+            echo "startTime : " . $startTime = $time[0]["startTime"];
+            echo "<br>";
+            echo "endTime : " . $endTime = $time[0]["endTime"];
+            echo "<br>";
+            break;
+    }
 }
 
 unset($json, $data);
 
-
-
-var_dump($Wx);
 $Wx = array();
-
