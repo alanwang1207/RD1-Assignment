8399c9ced7e4f54bfdc665bcf91d33ac7809d987
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-29 14:57:45 +0800

    兩天_ok

--- twodays_weather.php
+++ twodays_weather.php
@@ -13,42 +13,97 @@ $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?A
 $json = file_get_contents($url);  // 把整個文件讀入一個字符串中
 $data = json_decode($json, true);  // 將json轉成陣列或object 
 // var_dump($data);
-$weatherElement = $data['records']['location'][0]['weatherElement'];
+
+$weatherElement = $data['records']["locations"][0]['location'][0]['weatherElement'];
 // unset($json, $data);
 // var_dump(count($weatherElement));//記錄天氣因子個數
 for ($i = 0; $i < count($weatherElement); $i++) {
+    //元素名
     $elementName = $weatherElement[$i]["elementName"];
     echo "<br>";
+    //時間
     $time = $weatherElement[$i]["time"];
-    $parameterName = $time[0]["parameter"]["parameterName"];
-    // echo "startTime : " . $startTime = $time[0]["startTime"];
-    // echo "<br>";
-    // echo "endTime : " . $endTime = $time[0]["endTime"];
-    // echo "<br>";
-    switch ($elementName) {
-        case "Wx":
-            echo "目前天氣狀況 : " . $parameterName;
-            echo "<br>";
-            break;
-        case "PoP":
-            echo "降雨機率 : " . $parameterName . "%";
-            echo "<br>";
-            break;
-        case "MinT":
-            echo "最低溫度 : " . $parameterName . "°C";
-            echo "<br>";
-            break;
-        case "MaxT":
-            echo "最高溫度 : " . $parameterName . "°C";
-            echo "<br>";
-            break;
-        case "CI":
-            echo "舒適度 : " . $parameterName;
-            echo "<br>";
-            break;
+    //名稱
+    $description = $weatherElement[$i]["description"];
+    for ($j = 0; $j < count($time); $j++) {
+        $elementValue = $time[$j]["elementValue"][0]["value"];
+        switch ($elementName) {
+            case "PoP12h":
+                echo $description . " : " . $elementValue . "%";
+                echo "<br>";
+                echo "startTime : " . $startTime = $time[$j]["startTime"];
+                echo "<br>";
+                echo "endTime : " . $endTime = $time[$j]["endTime"];
+                echo "<br>";
+                break;
+            case "Wx":
+                echo $description . " : " . $elementValue;
+                echo "<br>";
+                echo "startTime : " . $startTime = $time[$j]["startTime"];
+                echo "<br>";
+                echo "endTime : " . $endTime = $time[$j]["endTime"];
+                echo "<br>";
+                break;
+
+            case "AT":
+                echo $description . " : " . $elementValue . "°C";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "T":
+                echo $description . " : " . $elementValue . "°C";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "RH":
+                echo $description . " : " . $elementValue . "%";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "CI":
+                $elementValue = $time[$j]["elementValue"][1]["value"];
+                echo $description . " : " . $elementValue;
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "WeatherDescription":
+                echo $description . " : " . $elementValue ;
+                echo "<br>";
+                echo "startTime : " . $startTime = $time[$j]["startTime"];
+                echo "<br>";
+                echo "endTime : " . $endTime = $time[$j]["endTime"];
+                echo "<br>";
+                break;
+            case "PoP6h":
+                echo $description . " : " . $elementValue . "%";
+                echo "<br>";
+                echo "startTime : " . $startTime = $time[$j]["startTime"];
+                echo "<br>";
+                echo "endTime : " . $endTime = $time[$j]["endTime"];
+                echo "<br>";
+                break;
+            case "WS":
+                echo $description . " : " . $elementValue . "公尺/秒";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "WD":
+                echo $description . " : " . $elementValue . "公尺/秒";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+            case "Td":
+                echo $description . " : " . $elementValue . "°C";
+                echo "<br>";
+                echo "dataTime : " . $startTime = $time[$j]["dataTime"];
+                echo "<br>";
+                break;
+        }
     }
 }
-
-unset($json, $data);
-
-$Wx = array();
