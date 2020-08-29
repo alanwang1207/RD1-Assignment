d838ff25aec9e1245540f8507b6461b1b752a2c9
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-29 11:02:44 +0800

    抓取時間_ok

--- current_weather.php
+++ current_weather.php
@@ -3,6 +3,7 @@ session_start();
 
 $cityName = $_SESSION['selectCity'];
 echo $cityName;
+echo "<br>";
 
 $api = "F-C0032-001";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
@@ -12,10 +13,26 @@ $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/".$api."?Authorization
 $json = file_get_contents($url);  // 把整個文件讀入一個字符串中
 $data = json_decode($json, true);  // 將json轉成陣列或object 
 // var_dump($data);
-$Wx = $data['records']['location'][0]['weatherElement'][0]["time"][0]["parameter"]["parameterValue"];
+$weatherElement = $data['records']['location'][0]['weatherElement'];
 // unset($json, $data);
-var_dump($Wx);
+// var_dump(count($weatherElement));//記錄天氣因子個數
+for($i=0;$i<count($weatherElement);$i++){
+    echo $elementName = $weatherElement[$i]["elementName"];
+    echo "<br>";
+    $time = $weatherElement[$i]["time"];
+    for($t=0;$t<count($time);$t++){
+        echo "startTime : ".$startTime = $time[$t]["startTime"];
+        echo "<br>";
+        echo "endTime : ".$endTime = $time[$t]["endTime"];
+        echo "<br>";
+        echo "天氣狀況 : ".$parameterName = $time[$t]["parameter"]["parameterName"];
+        echo "<br>";
+
+    }
+}
+
 unset($json, $data);
+
 
 
 var_dump($Wx);
