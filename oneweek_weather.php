6ff5c8c521a5f1ae9e8764e0d7b75528925a03fb
sz9751210 <sz9751210@gmail.com>
2020-08-30 21:15:11 +0800

    清空欄位_ok

--- oneweek_weather.php
+++ oneweek_weather.php
@@ -4,7 +4,11 @@ require("./config.php");
 $cityName = $_SESSION['selectCity'];
 echo $cityName;
 echo "<br>";
-
+//清空資料表欄位
+$sql = <<<sqlstate
+            DELETE FROM oneweek WHERE cityName='$cityName';
+        sqlstate;
+        mysqli_query($link, $sql);
 $resource_id = "F-D0047-091";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
@@ -26,7 +30,9 @@ foreach ($weatherElement[10]['time'] as $key => $value) {
     if ($value["startTime"] > $today) {
         $startTime = $value['startTime'];
         $weatherDescription = $value['elementValue'][0]['value'];
+        
         $sql = <<<sqlstate
+                   
                     insert into oneweek (cityName,startTime,weatherDescription)
                     values('$cityName','$startTime','$weatherDescription')
                   sqlstate;
