8c33d8b59f7e7d256661d4ca18306ba06485ded8
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-31 16:28:28 +0800

    修改資料表欄位

--- weather.sql
+++ weather.sql
@@ -4,29 +4,37 @@ use test3;
 
 
 CREATE TABLE `currentwt` (
-  `cityName` varchar(10)  NOT NULL,
+  `cityName` varchar(30)  NOT NULL,
   `Wx` varchar(30)  NOT NULL,
-  `PoP` varchar(10)  NOT NULL,
-  `MinT` varchar(10)  NOT NULL,
-  `MaxT` varchar(10)  NOT NULL,
-  `CI` varchar(20)  NOT NULL
+  `PoP` varchar(30)  NOT NULL,
+  `MinT` varchar(30)  NOT NULL,
+  `MaxT` varchar(30)  NOT NULL,
+  `CI` varchar(30)  NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   CREATE TABLE  `twodays` (
-  `cityName` varchar(10)  NOT NULL,
-  `startTime` DATETIME NOT NULL,
-  `weatherDescription` varchar(240)  NOT NULL
+  `cityName` varchar(30)  NOT NULL,
+  `Wx` varchar(30)  NOT NULL,
+  `PoP` varchar(30)  NOT NULL,
+  `T` varchar(30)  NOT NULL,
+  `CI` varchar(30)  NOT NULL,
+  `RH` varchar(30)  NOT NULL,
+  `startTime` DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `oneweek` (
-  `cityName` varchar(10)  NOT NULL,
-  `startTime` DATETIME NOT NULL,
-  `weatherDescription` varchar(240)  NOT NULL
+  `cityName` varchar(30)  NOT NULL,
+  `Wx` varchar(30)  NOT NULL,
+  `PoP` varchar(30)  NOT NULL,
+  `T` varchar(30)  NOT NULL,
+  `CI` varchar(30)  NOT NULL,
+  `RH` varchar(30)  NOT NULL,
+  `startTime` DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `rainfall` (
-  `locationName` varchar(10)  NOT NULL,
-  `city` varchar(10)  NOT NULL,
-  `onehour` varchar(20)  NOT NULL,
-  `HOUR_24` varchar(20)  NOT NULL
+  `locationName` varchar(30)  NOT NULL,
+  `city` varchar(30)  NOT NULL,
+  `onehour` varchar(30)  NOT NULL,
+  `HOUR_24` varchar(30)  NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
