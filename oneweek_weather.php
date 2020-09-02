d1f8e843454c9f527cdafe4f10b3cb435d6ec26b
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-02 14:37:08 +0800

    兩天註解_ok

--- oneweek_weather.php
+++ oneweek_weather.php
@@ -2,11 +2,14 @@
 session_start();
 require("./config.php");
 $cityName = $_SESSION['selectCity'];
+
 //清空資料表欄位
 $sql = <<<sqlstate
             DELETE FROM oneweek WHERE cityName='$cityName';
         sqlstate;
 mysqli_query($link, $sql);
+
+//拆解url
 $resource_id = "F-D0047-091";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
@@ -15,16 +18,12 @@ $locationName = urlencode($cityName);
 $url1 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=WeatherDescription&sort=time";  // Your json data url
 $json1 = file_get_contents($url1);  // 把整個文件讀入一個字符串中
 $data1 = json_decode($json1, true);  // 將json轉成陣列或object 
-
-
 $weatherElement1 = $data1['records']["locations"][0]['location'][0]['weatherElement'];
 
 //抓取天氣值
 $url2 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=Wx&sort=time";  // Your json data url
 $json2 = file_get_contents($url2);  // 把整個文件讀入一個字符串中
 $data2 = json_decode($json2, true);  // 將json轉成陣列或object 
-
-
 $weatherElement2 = $data2['records']["locations"][0]['location'][0]['weatherElement'];
 
 //用來判斷開始時間
--- twodays_weather.php
+++ twodays_weather.php
@@ -8,29 +8,29 @@ $sql = <<<sqlstate
             DELETE FROM twodays WHERE cityName='$cityName';
         sqlstate;
 mysqli_query($link, $sql);
+
+//拆解url
 $resource_id = "F-D0047-089";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
 
+//抓取天氣綜合描述
 $url1 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=WeatherDescription&sort=time";  // Your json data url
 $json1 = file_get_contents($url1);  // 把整個文件讀入一個字符串中
 $data1 = json_decode($json1, true);  // 將json轉成陣列或object 
-
-
 $weatherElement1 = $data1['records']["locations"][0]['location'][0]['weatherElement'];
 
+//抓取天氣值
 $url2 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=Wx&sort=time";  // Your json data url
 $json2 = file_get_contents($url2);  // 把整個文件讀入一個字符串中
 $data2 = json_decode($json2, true);  // 將json轉成陣列或object 
-
-
-
 $weatherElement2 = $data2['records']["locations"][0]['location'][0]['weatherElement'];
+
 //用來判斷開始時間
 $today = date('Y-m-d', strtotime("+1 day"));
 $twoday =  date('Y-m-d', strtotime("+3 day"));
-// 抓取天氣值
 
+//綜合描述所有值
 foreach ($weatherElement1[0]['time'] as $key => $value) {
     if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
         $startTime = $value['startTime'];
@@ -48,6 +48,8 @@ foreach ($weatherElement1[0]['time'] as $key => $value) {
         mysqli_query($link, $sql);
     }
 }
+
+//天氣所有值
 foreach ($weatherElement2[0]['time'] as $key => $value) {
     if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
         $startTime = $value['startTime'];
@@ -61,10 +63,9 @@ foreach ($weatherElement2[0]['time'] as $key => $value) {
     }
 }
 
-
+//秀出選取城市所有資料
 $sql = <<<sqlstate
 select * from twodays
 where (cityName = '$cityName') and (startTime like '%6:00%' or startTime like '%18:00%')
 sqlstate;
 $twodays = mysqli_query($link, $sql);
-?>
