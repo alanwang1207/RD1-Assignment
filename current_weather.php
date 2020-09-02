c0252c965085dd10dccaa2bba10ab11a51af72cc
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-02 14:31:17 +0800

    即時天氣註解_ok

--- current_weather.php
+++ current_weather.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
+//引入db設定值
 require("./config.php");
+//選取的城市名
 $cityName = $_SESSION['selectCity'];
-// echo $cityName;
-// echo "<br>";
+
 
 //清空資料表欄位
 $sql = <<<sqlstate
@@ -11,6 +12,7 @@ $sql = <<<sqlstate
         sqlstate;
 mysqli_query($link, $sql);
 
+//將url拆解
 $resource_id = "F-C0032-001";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
@@ -18,61 +20,44 @@ $locationName = urlencode($cityName);
 $url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&sort=time";  // Your json data url
 $json = file_get_contents($url);  // 把整個文件讀入一個字符串中
 $data = json_decode($json, true);  // 將json轉成陣列或object 
-// var_dump($data);
+
+//鎖定天氣元素
 $weatherElement = $data['records']['location'][0]['weatherElement'];
-// unset($json, $data);
-// var_dump(count($weatherElement));//記錄天氣因子個數
+
+//天氣元素分組
 for ($i = 0; $i < count($weatherElement); $i++) {
     $elementName = $weatherElement[$i]["elementName"];
-    // echo "<br>";
     $ntime = $weatherElement[$i]["time"][0];
     $parameterName = $ntime["parameter"]["parameterName"];
-    
-    // echo "startTime : " . $startTime = $time[0]["startTime"];
-    // echo "<br>";
-    // echo "endTime : " . $endTime = $time[0]["endTime"];
-    // echo "<br>";
     switch ($elementName) {
         case "Wx":
             $Wx = $parameterName;
             $WxValue = $ntime["parameter"]["parameterValue"];
-            // echo "目前天氣狀況 : " . $WxValue;
-            // echo "<br>";
             break;
         case "PoP":
             $PoP = $parameterName;
-            // echo "降雨機率 : " . $PoP . "%";
-            // echo "<br>";
             break;
         case "MinT":
             $MinT = $parameterName;
-            // echo "最低溫度 : " . $MinT . "°C";
-            // echo "<br>";
             break;
         case "MaxT":
             $MaxT = $parameterName;
-            // echo "最高溫度 : " . $parameterName . "°C";
-            // echo "<br>";
             break;
         case "CI":
             $CI = $parameterName;
-            // echo "舒適度 : " . $parameterName;
-            // echo "<br>";
             break;
     }
 }
 
+//輸入資料
 $sql = <<<sqlstate
     insert into currentwt (cityName,Wx,WxValue,PoP,MinT,MaxT,CI)
     values('$cityName','$Wx','$WxValue','$PoP','$MinT','$MaxT','$CI')
   sqlstate;
-
 mysqli_query($link, $sql);
 
+//秀出選取城市所有資料
 $sql = <<<sqlstate
     select * from currentwt where cityName = '$cityName';
 sqlstate;
 $currentwt = mysqli_query($link, $sql);
-
-?>
-
