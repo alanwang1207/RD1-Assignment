38fd52e07f80133f4685d95a3d02e44e4f668281
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-02 14:22:30 +0800

    兩天優化查詢速度

--- twodays_weather.php
+++ twodays_weather.php
@@ -12,12 +12,20 @@ $resource_id = "F-D0047-089";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
 $locationName = urlencode($cityName);
 
-$url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&sort=time";  // Your json data url
-$json = file_get_contents($url);  // 把整個文件讀入一個字符串中
-$data = json_decode($json, true);  // 將json轉成陣列或object 
+$url1 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=WeatherDescription&sort=time";  // Your json data url
+$json1 = file_get_contents($url1);  // 把整個文件讀入一個字符串中
+$data1 = json_decode($json1, true);  // 將json轉成陣列或object 
 // var_dump($data);
 
-$weatherElement = $data['records']["locations"][0]['location'][0]['weatherElement'];
+$weatherElement1 = $data1['records']["locations"][0]['location'][0]['weatherElement'];
+
+$url2 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=" . $locationName . "&elementName=Wx&sort=time";  // Your json data url
+$json2 = file_get_contents($url2);  // 把整個文件讀入一個字符串中
+$data2 = json_decode($json2, true);  // 將json轉成陣列或object 
+// var_dump($data);
+
+
+$weatherElement2 = $data2['records']["locations"][0]['location'][0]['weatherElement'];
 //用來判斷開始時間
 $today = date('Y-m-d', strtotime("+1 day"));
 $twoday =  date('Y-m-d', strtotime("+3 day"));
@@ -25,7 +33,7 @@ $twoday =  date('Y-m-d', strtotime("+3 day"));
 // var_dump(count($weatherElement));//記錄天氣因子個數
 // 抓取天氣值
 
-foreach ($weatherElement[6]['time'] as $key => $value) {
+foreach ($weatherElement1[0]['time'] as $key => $value) {
     if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
         $startTime = $value['startTime'];
         $weatherDescription = $value['elementValue'][0]['value'];
@@ -42,7 +50,7 @@ foreach ($weatherElement[6]['time'] as $key => $value) {
         mysqli_query($link, $sql);
     }
 }
-foreach ($weatherElement[1]['time'] as $key => $value) {
+foreach ($weatherElement2[0]['time'] as $key => $value) {
     if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
         $startTime = $value['startTime'];
         $weatherDescription = $value['elementValue'][0]['value'];
