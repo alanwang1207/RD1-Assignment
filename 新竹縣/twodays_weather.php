8559ab842cb418064ab3ea199a66673c93d78ee2
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-04 15:17:59 +0800

    修正url

--- 新竹縣/twodays_weather.php
+++ 新竹縣/twodays_weather.php
@@ -5,10 +5,6 @@ require("../config.php");
 //選取的鄉鎮市區名
 $locationName =  $_SESSION['selectLocation'];
 $cityName = $selectCity;
-// echo $locationName;
-// $_POST['selectLocation'] = '宜蘭縣';
-
-
 //清空資料表欄位
 $sql = <<<sqlstate
             DELETE FROM twodays WHERE locationName='$locationName';
@@ -19,30 +15,28 @@ mysqli_query($link, $sql);
 //將url拆解
 $resource_id = "F-D0047-009";
 $Authorization = "CWB-20260A47-5D47-474D-AABA-BBC6BC84F310";
+$locationNameurl = urlencode($locationName);
 
 //抓取天氣綜合描述
-$url1 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&elementName=WeatherDescription&sort=time";  // Your json data url
+$url1 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=".$locationNameurl."&elementName=WeatherDescription&sort=time";  // Your json data url
 $json1 = file_get_contents($url1);  // 把整個文件讀入一個字符串中
 $data1 = json_decode($json1, true);  // 將json轉成陣列或object 
-$location1 = $data1['records']['locations'][0]['location'];
+$weatherElement1 = $data1['records']['locations'][0]['location'][0]["weatherElement"];
 
 //抓取天氣值
-$url2 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&elementName=Wx&sort=time";  // Your json data url
+$url2 = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/" . $resource_id . "?Authorization=" . $Authorization . "&format=JSON&locationName=".$locationNameurl."&elementName=Wx&sort=time";  // Your json data url
 $json2 = file_get_contents($url2);  // 把整個文件讀入一個字符串中
 $data2 = json_decode($json2, true);  // 將json轉成陣列或object 
-$location2 = $data2['records']['locations'][0]['location'];
+$weatherElement2 = $data2['records']["locations"][0]['location'][0]['weatherElement'];
 
 //用來判斷開始時間
 $today = date('Y-m-d', strtotime("+1 day"));
 $twoday =  date('Y-m-d', strtotime("+3 day"));
 
 
-for ($i = 0; $i < count($location1); $i++) {
-    // $locationName = $location[$i]['locationName'];
 
-    if ($location1[$i]["locationName"] == $locationName) {
         //綜合描述所有值
-        foreach ($location1[$i]["weatherElement"][0]['time'] as $key => $value) {
+        foreach ($weatherElement1[0]['time'] as $key => $value) {
 
             if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
                 $startTime = $value['startTime'];
@@ -61,7 +55,7 @@ for ($i = 0; $i < count($location1); $i++) {
         }
 
         //天氣所有值
-        foreach ($location2[$i]["weatherElement"][0]['time'] as $key => $value) {
+        foreach ($weatherElement2[0]['time'] as $key => $value) {
 
 
             if ($value["startTime"] > $today && $twoday > $value["startTime"]) {
@@ -75,8 +69,7 @@ for ($i = 0; $i < count($location1); $i++) {
                 mysqli_query($link, $sql);
             }
         }
-    }
-}
+
 
 
 $sql = <<<sqlstate
